e97ab884bb62889ec47716baf0529a2329d42c28
Milene <milene.jannetti@gmail.com>
2022-04-02 01:53:05 -0300

    Tela de confirmação + perguntar nome e endereço

--- script.js
+++ script.js
@@ -55,16 +55,50 @@ function selecionar(el) {
     }
 }
 
-function fecharPedido(){
+function confirmar() {
+    document.querySelector(".confirmacao").classList.add("aparecer");
     const nomePrato = document.querySelector(".PratoSelecionado h3").innerHTML;
-    const nomeBebida = document.querySelector(".BebidaSelecionada h3").innerHTML;
-    const nomeSobrem = document.querySelector(".SobremSelecionada h3").innerHTML;
-    const precoPrato = Number(document.querySelector(".PratoSelecionado h4").innerHTML.replace(/R[^\d.]/g, '').replace(/,/g,'.'));
-    const precoBebida = Number(document.querySelector(".BebidaSelecionada h4").innerHTML.replace(/R[^\d.]/g, '').replace(/,/g,'.'));
-    const precoSobrem = Number(document.querySelector(".SobremSelecionada h4").innerHTML.replace(/R[^\d.]/g, '').replace(/,/g,'.'));
-    let precoTotal = precoPrato+precoBebida+precoSobrem;
-    precoTotal = precoTotal.toFixed([2]);
-    const textoMsg = "Olá, gostaria de fazer o pedido:\n- Prato: "+nomePrato+"\n- Bebida: "+nomeBebida+"\n- Sobremesa: "+nomeSobrem+"\nTotal: R$ "+precoTotal;
+const nomeBebida = document.querySelector(".BebidaSelecionada h3").innerHTML;
+const nomeSobrem = document.querySelector(".SobremSelecionada h3").innerHTML;
+let precoPratostr = document.querySelector(".PratoSelecionado h4").innerHTML.replace(/R[^\d.]/g, '');
+let precoBebidastr = document.querySelector(".BebidaSelecionada h4").innerHTML.replace(/R[^\d.]/g, '');
+let precoSobremstr = document.querySelector(".SobremSelecionada h4").innerHTML.replace(/R[^\d.]/g, '');
+precoPrato = Number(precoPratostr.replace(/,/g,'.'));
+precoBebida = Number(precoBebidastr.replace(/,/g,'.'));
+precoSobrem = Number(precoSobremstr.replace(/,/g,'.'));
+let precoTotal = precoPrato+precoBebida+precoSobrem;
+precoTotal = precoTotal.toFixed([2]);
+    precoTotal = precoTotal.toString();
+    precoTotal = "R$ "+ precoTotal;
+    document.querySelector(".checkPedido").querySelector(".prato p").innerHTML = nomePrato;
+    document.querySelector(".checkPedido").querySelector(".bebida p").innerHTML = nomeBebida;
+    document.querySelector(".checkPedido").querySelector(".sobremesa p").innerHTML = nomeSobrem;
+    document.querySelector(".checkPedido").querySelector(".prato p:last-child").innerHTML = precoPratostr;
+    document.querySelector(".checkPedido").querySelector(".bebida p:last-child").innerHTML = precoBebidastr;
+    document.querySelector(".checkPedido").querySelector(".sobremesa p:last-child").innerHTML = precoSobremstr;
+    document.querySelector(".checkPedido").querySelector(".preço").innerHTML = precoTotal;
+}
+function voltar() {
+    document.querySelector(".confirmacao").classList.remove("aparecer");
+}
+
+function fecharPedido(){
+    const nomeCliente = prompt("Infome seu nome");
+    const endereco = prompt("Infome o endereço para entrega");
+    const nomePrato = document.querySelector(".PratoSelecionado h3").innerHTML;
+const nomeBebida = document.querySelector(".BebidaSelecionada h3").innerHTML;
+const nomeSobrem = document.querySelector(".SobremSelecionada h3").innerHTML;
+let precoPratostr = document.querySelector(".PratoSelecionado h4").innerHTML.replace(/R[^\d.]/g, '');
+let precoBebidastr = document.querySelector(".BebidaSelecionada h4").innerHTML.replace(/R[^\d.]/g, '');
+let precoSobremstr = document.querySelector(".SobremSelecionada h4").innerHTML.replace(/R[^\d.]/g, '');
+precoPrato = Number(precoPratostr.replace(/,/g,'.'));
+precoBebida = Number(precoBebidastr.replace(/,/g,'.'));
+precoSobrem = Number(precoSobremstr.replace(/,/g,'.'));
+let precoTotal = precoPrato+precoBebida+precoSobrem;
+precoTotal = precoTotal.toFixed([2]);
+    const textoMsg = "Olá, gostaria de fazer o pedido:\n- Prato: "+nomePrato+
+    "\n- Bebida: "+nomeBebida+"\n- Sobremesa: "+nomeSobrem+"\nTotal: R$ "+precoTotal+
+    "\n\nNome: "+nomeCliente+"\nEndereço: "+endereco;
     let msgPronta = encodeURIComponent(textoMsg);
     location.href = `https://wa.me/525951207831?text=${msgPronta}`;
 }
